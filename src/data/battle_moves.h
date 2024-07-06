8e29c8975d63ff3326a5ede46c00150c30384541
xilfer123 <zaidiadlan@yahoo.ca>
2024-07-06 15:07:54 -0400

    Update battle_moves.h

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -9796,7 +9796,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .target = MOVE_TARGET_SELECTED,
         .priority = 0,
         .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGSROCK_AFFECTED | FLAG_SHEER_FORCE_BOOST,
-        .split = SPLIT_SPECIAL,
+        .split = SPLIT_PHYSICAL,
     },
 
     [MOVE_POWER_TRIP] =
